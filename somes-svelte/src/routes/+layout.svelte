44d153da2595628e1ee10db16ad49f347e415348
elftausend <76885970+elftausend@users.noreply.github.com>
2023-04-03 20:32:46 +0200

    Update svelte

--- somes-svelte/src/routes/+layout.svelte
+++ somes-svelte/src/routes/+layout.svelte
@@ -1,11 +1,51 @@
-<script>
-    // Your selected Skeleton theme:
-    import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+<script lang='ts'>
+	// The ordering of these imports is critical to your app working properly
+	// import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	import '../theme.postcss';
 
-    // This contains the bulk of Skeletons required styles:
-    import '@skeletonlabs/skeleton/styles/all.css';
-
-    // Finally, your application's global stylesheet (sometimes labeled 'app.css')
-    import '../app.postcss';
-                            
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/all.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 </script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">Skeleton</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://discord.gg/EXqV7W8MtY"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Discord
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://twitter.com/SkeletonUI"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Twitter
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/skeletonlabs/skeleton"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
--- somes-svelte/tailwind.config.cjs
+++ somes-svelte/tailwind.config.cjs
@@ -1,21 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	// 1. Apply the dark mode class setting:
 	darkMode: 'class',
-	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-		// 2. Append the path for the Skeleton NPM package and files:
-		require('path').join(require.resolve(
-			'@skeletonlabs/skeleton'),
-			'../**/*.{html,js,svelte,ts}'
-		)
-	],
+	content: ['./src/**/*.{html,js,svelte,ts}', require('path').join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
 	theme: {
 		extend: {},
 	},
-	plugins: [
-		// require('@tailwindcss/forms'),
-		// 3. Append the Skeleton plugin to the end of this list
-		...require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')()
-	]
+	plugins: [...require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')()],
 }
