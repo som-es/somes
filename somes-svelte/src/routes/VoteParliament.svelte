f29ad7eef6edfc7c70ccd3de4c5de70ab3f117fc
elftausend <76885970+elftausend@users.noreply.github.com>
2023-06-21 11:10:49 +0200

    Remove unused VoteParliament map code

--- somes-svelte/src/routes/VoteParliament.svelte
+++ somes-svelte/src/routes/VoteParliament.svelte
@@ -17,24 +17,7 @@
     function findDelegateFromId(id: number): Delegate | undefined {
         return dels.find(del => del.id === id);
     }
-
-    /*let otherPartyToColor = new Map<string, {color: string}[]>();
-    otherPartyToColor.set("SPÖ", [{color:"#f48992"}, {color:"#E31E2D"}]);
-    otherPartyToColor.set("ÖVP", [{color:"#567a7f"}, {color:"#62C3D0"}]);
-    otherPartyToColor.set("FPÖ", [{color:"#354776"}, {color:"#0052FB"}]);
-    otherPartyToColor.set("GRÜNE", [{color:"#537239"}, {color:"#69B12E"}]);
-    otherPartyToColor.set("NEOS", [{color:"#934b6c"}, {color:"#E3257B"}]);*/
-
-    /*let partyToColorMap = new Map<string, {color: string, infavor: boolean}>();
-    // better
-    // partyToColorMap.set("SPÖ", otherPartyToColor.get("SPÖ")[isPartyInFavor("SPÖ") ? 0 : 1]);
-
-    partyToColorMap.set("SPÖ", isPartyInFavor("SPÖ") ?     {color:"#E31E2D", infavor:true} : {color:"#f48992", infavor:false});
-    partyToColorMap.set("ÖVP", isPartyInFavor("ÖVP") ?     {color:"#62C3D0", infavor:true} : {color:"#567a7f", infavor:false});
-    partyToColorMap.set("FPÖ", isPartyInFavor("FPÖ") ?     {color:"#0052FB", infavor:true} : {color:"#354776", infavor:false});
-    partyToColorMap.set("GRÜNE", isPartyInFavor("GRÜNE") ? {color:"#69B12E", infavor:true} : {color:"#537239", infavor:false});
-    partyToColorMap.set("NEOS", isPartyInFavor("NEOS") ?   {color:"#E3257B", infavor:true} : {color:"#934b6c", infavor:false});*/
-    
+ 
     let circles2d: Bubble[][] = setupParliament(seats, width, height, 6.9);
     let selected: Bubble;
 
@@ -46,13 +29,6 @@
         selected = bubble;
         console.log(selected);
 	}
-
-    /*function partyToColor(party: string): string { 
-        if (partyToColorMap.has(party)) {
-            return partyToColorMap.get(party)?.color as string;
-        }
-        return "rgb(196, 180, 189)";
-    }*/
 
     const partyToColorMap = getPartyToColor();
 
@@ -100,17 +76,6 @@
         if (del.seat_col == null || del.seat_row == null) {
             return;
         }
-
-        /*const biColors = otherPartyToColor.get(del.party);
-
-        if (biColors == null) {
-            return;
-        }*/
-
-        // select delegates.name,delegates.party,speeches.infavor from speeches inner join delegates on speeches.delegate_id=delegates.id where legislative_initiatives_id=14;
-        // if speaker is against partei linie
-        
-        // circles2d[del.seat_row-1][del.seat_col-1].color = biColors[speech.infavor ? 1 : 0].color;
 
         if (speech.infavor) {
             circles2d[del.seat_row-1][del.seat_col-1].opacity = 1.0;            
