24b7179d029904336d80c41d18f5e2e2a0858290
Davideko <david.koch.email@gmail.com>
2023-08-26 19:25:42 +0200

    [somes-svelte] Tweaked navbar size

--- somes-svelte/src/routes/+layout.svelte
+++ somes-svelte/src/routes/+layout.svelte
@@ -28,10 +28,10 @@
 <AppShell>
 	<!-- TODO: show the second header below the the first while also making it sticky (without the f one being sticky) -->
 	<svelte:fragment slot="header">
-		<AppBar background="bg-inherit">
+		<AppBar background="bg-inherit" padding="p-2">
 			<svelte:fragment slot="lead">
 				<a class="mx-4" href="/">
-					<svg class="fill-current stroke-current" xmlns="http://www.w3.org/2000/svg" width="20vh" viewBox="0 0 2244.417 622.549">
+					<svg class="fill-current stroke-current" xmlns="http://www.w3.org/2000/svg" width="16vh" viewBox="0 0 2244.417 622.549">
 						<g id="Gruppe_76" data-name="Gruppe 76" transform="translate(7925.417 -13505)">
 						  <g id="Logo" transform="translate(-7910.417 13566.545)">
 							<path id="Pfad_1" data-name="Pfad 1" d="M853.355,224.34,830.6,254.505H411.945L389.19,224.34Z" transform="translate(-378.445 -224.34)" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="30"/>
@@ -50,9 +50,9 @@
 				<LightSwitch />
 			</svelte:fragment>
 		</AppBar>
-		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end" padding="p-2">
 			<svelte:fragment slot="lead">
-				<a href="">
+				<a href="" class="pl-[25vw]">
 					<div>
 						<svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
 							<path id="Pfad_9" data-name="Pfad 9" d="M4.113,18.111a3.365,3.365,0,0,0-.352-3.27,7.449,7.449,0,0,1-1.533-4.529,7.5,7.5,0,1,1,7.5,7.5A7.61,7.61,0,0,1,7.957,17.6a1.851,1.851,0,0,0-1.439.246q-.288.185-.591.352a13.452,13.452,0,0,1-2.339,1.055c.131-.27.253-.533.37-.8.052-.111.1-.229.15-.346Zm-4.134-7.8a10.646,10.646,0,0,0,2.151,6.463c-.042.1-.089.205-.131.3A16.807,16.807,0,0,1,.284,20.127a1.684,1.684,0,0,0-.216,1.518A1.154,1.154,0,0,0,1.1,22.5a12.082,12.082,0,0,0,5.751-1.74c.225-.129.45-.264.666-.4a9.155,9.155,0,0,0,2.208.264,10.042,10.042,0,0,0,9.75-10.312A10.042,10.042,0,0,0,9.729,0,10.042,10.042,0,0,0-.021,10.313ZM20.228,28.125a9.487,9.487,0,0,0,2.208-.264c.216.135.441.27.666.4A12.082,12.082,0,0,0,28.853,30a1.146,1.146,0,0,0,1.031-.85,1.665,1.665,0,0,0-.216-1.518,17.387,17.387,0,0,1-1.716-3.053c-.042-.1-.089-.2-.131-.3a10.65,10.65,0,0,0,2.156-6.469,10.076,10.076,0,0,0-9.29-10.3,13.274,13.274,0,0,1,.291,2.8v.035a7.546,7.546,0,0,1,6.75,7.465A7.42,7.42,0,0,1,26.2,22.336a3.367,3.367,0,0,0-.352,3.27c.052.117.1.234.15.346.117.264.244.527.37.8a13.029,13.029,0,0,1-2.339-1.055q-.3-.167-.591-.352A1.851,1.851,0,0,0,22,25.1a7.473,7.473,0,0,1-8.184-3.439,10.821,10.821,0,0,1-2.344.691A9.726,9.726,0,0,0,20.228,28.125Z" transform="translate(0.022)"/>
@@ -71,7 +71,7 @@
 				</div>
 			</a>
 			<svelte:fragment slot="trail">
-				<a href={profileLink}>
+				<a href={profileLink} class="pr-[25vw]">
 					<svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
 						<path id="Pfad_8" data-name="Pfad 8" d="M15,15c4.734,0,8.571-3.358,8.571-7.5S19.734,0,15,0,6.429,3.358,6.429,7.5,10.266,15,15,15Zm-3.06,2.812C5.344,17.812,0,22.488,0,28.26A1.877,1.877,0,0,0,1.989,30H28.011A1.877,1.877,0,0,0,30,28.26c0-5.771-5.344-10.447-11.94-10.447Z"/>
 					</svg>                  
@@ -81,14 +81,3 @@
 	</svelte:fragment>
 	<slot />
 </AppShell>
-
-<style>
-.navbar {
-	position: sticky;
-	top: 0;
-	z-index: 9;
-	display: flex;
-	justify-content: space-around;
-	padding: 10px;
-}
-</style>
