c61eebbd7aea256f9a98534db12a2bc1af188478
Davideko <david.koch.email@gmail.com>
2023-09-01 12:35:59 +0200

    [somes-svelte] Remove debug lang switch

--- somes-svelte/src/components/LangSwitch.svelte
+++ somes-svelte/src/components/LangSwitch.svelte
@@ -10,11 +10,6 @@
 	};
 </script>
 
-<select bind:value={$locale} on:change={handleChange}>
-	{#each $locales as value}
-		<option {value}>{$t(`lang.${value}`)}</option>
-	{/each}
-</select>
 <RadioGroup rounded="rounded-container-token" padding="px-2">
 	<RadioItem bind:group={$locale} name="justify" value={"de"} on:change={handleChange}>
 		<Icon name="at" />
