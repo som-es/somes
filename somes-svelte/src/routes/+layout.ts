96bce2c26687447e6b96a31a46870febad43aade
elftausend <76885970+elftausend@users.noreply.github.com>
2024-01-15 18:22:38 +0100

    git add .! WARN - UPDATED BASE PATH git add .

--- somes-svelte/src/routes/+layout.ts
+++ somes-svelte/src/routes/+layout.ts
@@ -1,6 +1,8 @@
 import { addTranslations, setLocale, setRoute } from '$lib/translations';
 import type { Load } from '@sveltejs/kit';
 
+export const prerender = true;
+
 export const load: Load = async ({ data }: { data: any }) => {
 	const { i18n, translations } = data;
 	const { locale, route } = i18n;
--- somes-svelte/svelte.config.js
+++ somes-svelte/svelte.config.js
@@ -1,23 +1,39 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [
-		vitePreprocess(),
-		preprocess({
-			postcss: true,
-		}),
-	],
+        preprocess: [
+                vitePreprocess(),
+                preprocess({
+                        postcss: true,
+                }),
+        ],
 
-	kit: {
-		adapter: adapter({
-			pages: "./build/html",
-			assets: "./build/html",
-			fallback: "200.html",
-		}),
-	},
+        kit: {
+                adapter: adapter({
+                        pages: 'build',
+                        assets: 'build',
+                        fallback: null,
+                        precompress: false,
+                        strict: true
+                }),
+                paths: {
+                        base: '/alpha'
+                },
+		prerender: {
+			handleHttpError: ({ path, referrer, message }) => {
+				// ignore deliberate link to shiny 404 page
+				if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
+					return;
+				}
+
+				// otherwise fail the build
+				// throw new Error(message);
+			}
+		}
+        },
 };
 
 export default config;
