697acf4a8efeebd1fdde9ac344881fcd2bec28c5
elftausend <76885970+elftausend@users.noreply.github.com>
2024-07-17 22:58:33 +0200

    Update +page.svelte

--- somes-svelte-rework/src/routes/delegates/+page.svelte
+++ somes-svelte-rework/src/routes/delegates/+page.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
-
-
+    let search_valeue: string;
+    obMount(async () => {
+            
+        )
+    );
 </script>
 
 <div>
     delegates
+    <input bind={search_value} type="text" />
+    <VoteParliament />
 </div>
