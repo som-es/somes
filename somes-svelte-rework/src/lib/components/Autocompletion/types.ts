b8bf39f2042a9f963e1451456b2970d674775b3b
elftausend <76885970+elftausend@users.noreply.github.com>
2024-07-18 15:30:41 +0200

    Add party at autocomplete, custom autocomplete component from skeleton

---
+++ somes-svelte-rework/src/lib/components/Autocompletion/types.ts
@@ -0,0 +1,13 @@
+// Autocomplete Types
+
+export interface AutocompleteOption<Value = unknown, Meta = unknown> {
+	/** Provide a unique display label per option. Supports HTML. */
+	label: string;
+    right_label: string;
+	/** Provide a unique option value. */
+	value: Value;
+	/** Provide a comma separated list of keywords. */
+	keywords?: string;
+	/** Pass arbitrary data per option. */
+	meta?: Meta;
+}
