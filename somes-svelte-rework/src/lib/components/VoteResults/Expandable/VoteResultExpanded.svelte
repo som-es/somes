4753473b5255c4964f4dbfc73ee8b0514ace96b9
elftausend <76885970+elftausend@users.noreply.github.com>
2024-08-02 22:32:09 +0200

    add badge to emphasis list items

--- somes-svelte-rework/src/lib/components/VoteResults/Expandable/VoteResultExpanded.svelte
+++ somes-svelte-rework/src/lib/components/VoteResults/Expandable/VoteResultExpanded.svelte
@@ -41,9 +41,12 @@
     {#if emphasis}
         {#if emphasis.length > 0}
             <div class="emphasis-item bg-primary-300 px-10">
-                <ul class="mt-1">
+                <ul class="mt-1 list fill-primary-400">
                     {#each emphasis as emph}
-                        <li>- {emph}</li>
+                        <li>
+                            <span class="badge bg-primary-500"></span>
+                            <span>{emph}</span>
+                        </li>
                     {/each}
                 </ul>
             </div>
