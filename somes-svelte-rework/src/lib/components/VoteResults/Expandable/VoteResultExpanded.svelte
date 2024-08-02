8075b9a8544b6d2bd07d3ad0446c58476bc5bcac
elftausend <76885970+elftausend@users.noreply.github.com>
2024-08-03 01:11:56 +0200

    Add InfoTiles.svelte

--- somes-svelte-rework/src/lib/components/VoteResults/Expandable/VoteResultExpanded.svelte
+++ somes-svelte-rework/src/lib/components/VoteResults/Expandable/VoteResultExpanded.svelte
@@ -9,6 +9,7 @@
 	import SimpleDonut from "$lib/components/UI/SimpleDonut.svelte";
 	import { partyToColor } from "$lib/partyColor";
 	import Emphasis from "../Emphasis/Emphasis.svelte";
+	import InfoTiles from "../InfoTiles/InfoTiles.svelte";
 
 	export let voteResult: VoteResult;
 	export let dels: Delegate[];
@@ -17,63 +18,17 @@
 		?.split('\n\t')
 		.filter((x) => x.length > 0);
 
-    function dashDateToDotDate(date: string): string {
-        const dateParts = date.split('-');
-
-        return `${dateParts[2]}.${dateParts[1]}.${dateParts[0]}`
-    }
-
     const whichGridContainer = emphasis == null ? "grid-container-without-emphasis" : "grid-container-with-emphasis";
-
-    const NOT_REACHED_COLOR = 'rgb(var(--color-primary-600))';
-
-    const conicStopsSimpleMajority: ConicStop[] = [
-        { color: 'rgb(var(--color-secondary-400))', start: 0, end: 180 },
-        { color: NOT_REACHED_COLOR, start: 180, end: 360 },
-    ];
-
-    const conicStopsOtherMajority: ConicStop[] = [
-        { color: 'rgb(var(--color-secondary-400))', start: 0, end: 240 },
-        { color: NOT_REACHED_COLOR, start: 240, end: 360 },
-    ];
-
-    function generateConicStopsForAchievedVotes(): ConicStop[] {
-        voteResult.votes.sort((a, b) => b.fraction - a.fraction);
-        let fractionSum = 0;
-        voteResult.votes.forEach((vote) => {
-            fractionSum += vote.fraction;
-        });
-        let currentStart = 0;
-
-        let conicStops = [];
-
-        for (let i = 0; i < voteResult.votes.length; i++) {
-            let vote = voteResult.votes[i];
-            if (!vote.infavor) {
-                continue
-            }
-            const share = (vote.fraction / fractionSum) * 360;
-            const prevStart = currentStart;
-            currentStart += share;
-            conicStops.push({color: partyToColor(vote.party), start: prevStart, end: currentStart});
-        }
-
-        if (conicStops.length != voteResult.votes.length) {
-            conicStops.push({color: NOT_REACHED_COLOR, start: currentStart, end: 360 - currentStart});
-        }
-
-        return conicStops;
-    }
-    const conicsStopsAchievedVotes = generateConicStopsForAchievedVotes();
-
 </script>
 <div class="lg:!hidden entry bg-primary-200 dark:bg-primary-400 mt-3">
     <Emphasis emphasis={emphasis} /> 
-    <div class="rounded-md w-full">
+    <div class="rounded-md w-full bg-primary-100 parliament-item mt-3 mb-3">
         <VoteParliament {dels} {voteResult} preview={true} />
     </div>
 
-    <div class="flex justify-between"> 
+    <InfoTiles voteResult={voteResult} />
+
+    <div class="flex justify-between mt-3"> 
         <div class="accepted-item bg-primary-300">Angenommen: {voteResult.legislative_initiative.accepted}</div>
         <div class="ml-auto more-info-item"><SButton class="bg-tertiary-500">Details anzeigen</SButton></div>
     </div>
@@ -91,61 +46,7 @@
     <div class="rounded-md min-w-full max-w-full ml-auto  parliament-item bg-primary-100">
         <VoteParliament {dels} {voteResult} preview={true} />
     </div>
-    <div class="flex flex-wrap info-item gap-3">
-
-        <div class="responsive-accepted-hidden accepted-item square bg-primary-300">
-            <div class="flex flex-col items-center justify-center">
-                {#if voteResult.legislative_initiative.accepted}	
-                    {@html checkmarkIcon}
-                    <div>
-                        Angenommen
-                    </div>
-                {:else}
-                    {@html crossmarkIcon}
-                    <div>
-                        Abgelehnt
-                    </div>
-                {/if}
-            </div>
-            
-            <!-- Angenommen: {voteResult.legislative_initiative.accepted} -->
-        </div>
-        <div class="majority-item square flex bg-primary-300">
-            <div class="flex flex-col items-center justify-center">
-                <SimpleDonut stops={voteResult.legislative_initiative.requires_simple_majority ? conicStopsSimpleMajority : conicStopsOtherMajority} />
-                <div>
-                    Notwendige
-                </div>
-                <div>
-                    Mehrheit
-                </div>
-
-            </div> 
-        </div>
-        <div class="square flex bg-primary-300">
-            <div class="flex flex-col items-center justify-center">
-                <SimpleDonut stops={conicsStopsAchievedVotes} />
-                <div>
-                    Erreichte
-                </div>
-                <div>
-                    Mehrheit
-                </div>
-                <!-- {voteResult.legislative_initiative.requires_simple_majority ? "1/2" : "2/3" } -->
-
-            </div> 
-        </div>
-        <div class="accepted-item square bg-primary-300">
-            <div class="flex flex-col items-center justify-center">
-                <div class="bold font-bold text-lg">
-                    {dashDateToDotDate(voteResult.legislative_initiative.created_at.toString())}
-                </div>
-                <div>
-                    Abgestimmt am 
-                </div>
-            </div>
-        </div>
-    </div>
+    <InfoTiles voteResult={voteResult} />
     <div class="ml-auto details-item mt-auto"><SButton class="bg-tertiary-500">Details anzeigen</SButton></div>
 </div>
 
@@ -178,18 +79,6 @@
 		padding: 10px;
 	}
 
-	.square {
-		aspect-ratio: 1/ 1;
-        min-width: 140px;
-        min-height: 140px;
-        max-height: 140px;
-		/* padding: 5%; */
-        display: flex;
-        justify-content: center;
-        align-content: center;
-        border-radius: 1rem;
-	}
-
 	.parliament-item {
 		grid-area: p;
 		border-radius: 2rem;
@@ -219,12 +108,6 @@
 		grid-area: dt;
 	}
 
-	.info-item {
-		grid-area: i;
-        overflow: hidden;  /* NEW */
-        min-width: 0;      /* NEW; needed for Firefox */
-	}
-
 	.details-item {
 		grid-area: d;
 	}
@@ -232,10 +115,4 @@
 	.item {
 		grid-column: 1fr;
 	}
-    @media not all and (min-width: 1254px) {
-    .responsive-accepted-hidden {
-        display: none !important;
-    }
-}
-
 </style>
---
+++ somes-svelte-rework/src/lib/components/VoteResults/InfoTiles/InfoTiles.svelte
@@ -0,0 +1,160 @@
+<script lang="ts">
+	import SimpleDonut from "$lib/components/UI/SimpleDonut.svelte";
+	import crossmarkIcon from '$lib/assets/misc_icons/crossmark.svg?raw';
+	import checkmarkIcon from '$lib/assets/misc_icons/checkmark.svg?raw';
+	import type { ConicStop } from "@skeletonlabs/skeleton";
+	import { partyToColor } from "$lib/partyColor";
+	import type { VoteResult } from "$lib/types";
+
+    export let voteResult: VoteResult;
+    
+    function dashDateToDotDate(date: string): string {
+        const dateParts = date.split('-');
+
+        return `${dateParts[2]}.${dateParts[1]}.${dateParts[0]}`
+    }
+
+    const NOT_REACHED_COLOR = 'rgb(var(--color-primary-600))';
+
+    const conicStopsSimpleMajority: ConicStop[] = [
+        { color: 'rgb(var(--color-secondary-400))', start: 0, end: 180 },
+        { color: NOT_REACHED_COLOR, start: 180, end: 360 },
+    ];
+
+    const conicStopsOtherMajority: ConicStop[] = [
+        { color: 'rgb(var(--color-secondary-400))', start: 0, end: 240 },
+        { color: NOT_REACHED_COLOR, start: 240, end: 360 },
+    ];
+
+    function generateConicStopsForAchievedVotes(): ConicStop[] {
+        voteResult.votes.sort((a, b) => b.fraction - a.fraction);
+        let fractionSum = 0;
+        voteResult.votes.forEach((vote) => {
+            fractionSum += vote.fraction;
+        });
+        let currentStart = 0;
+
+        let conicStops = [];
+
+        for (let i = 0; i < voteResult.votes.length; i++) {
+            let vote = voteResult.votes[i];
+            if (!vote.infavor) {
+                continue
+            }
+            const share = (vote.fraction / fractionSum) * 360;
+            const prevStart = currentStart;
+            currentStart += share;
+            conicStops.push({color: partyToColor(vote.party), start: prevStart, end: currentStart});
+        }
+
+        if (conicStops.length != voteResult.votes.length) {
+            conicStops.push({color: NOT_REACHED_COLOR, start: currentStart, end: 360 - currentStart});
+        }
+
+        return conicStops;
+    }
+    const conicsStopsAchievedVotes = generateConicStopsForAchievedVotes();
+
+</script>
+
+<div class="flex flex-wrap info-item gap-3">
+    <div class="responsive-accepted-hidden accepted-item square bg-primary-300">
+        <div class="flex flex-col items-center justify-center">
+            {#if voteResult.legislative_initiative.accepted}	
+                {@html checkmarkIcon}
+                <div>
+                    Angenommen
+                </div>
+            {:else}
+                {@html crossmarkIcon}
+                <div>
+                    Abgelehnt
+                </div>
+            {/if}
+        </div>
+        
+        <!-- Angenommen: {voteResult.legislative_initiative.accepted} -->
+    </div>
+    <div class="majority-item square flex bg-primary-300">
+        <div class="flex flex-col items-center justify-center">
+            <SimpleDonut stops={voteResult.legislative_initiative.requires_simple_majority ? conicStopsSimpleMajority : conicStopsOtherMajority} />
+            <div>
+                Notwendige
+            </div>
+            <div>
+                Mehrheit
+            </div>
+
+        </div> 
+    </div>
+    <div class="square flex bg-primary-300">
+        <div class="flex flex-col items-center justify-center">
+            <SimpleDonut stops={conicsStopsAchievedVotes} />
+            <div>
+                Erreichte
+            </div>
+            <div>
+                Mehrheit
+            </div>
+            <!-- {voteResult.legislative_initiative.requires_simple_majority ? "1/2" : "2/3" } -->
+
+        </div> 
+    </div>
+    <div class="accepted-item square bg-primary-300">
+        <div class="flex flex-col items-center justify-center">
+            <div class="bold font-bold text-lg">
+                {dashDateToDotDate(voteResult.legislative_initiative.created_at.toString())}
+            </div>
+            <div>
+                Abgestimmt am 
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+	.square {
+		aspect-ratio: 1/ 1;
+        min-width: 140px;
+        min-height: 140px;
+        max-height: 140px;
+		/* padding: 5%; */
+        display: flex;
+        justify-content: center;
+        align-content: center;
+        border-radius: 1rem;
+	}
+
+	.accepted-item {
+		grid-area: a;
+	}
+
+	.majority-item {
+		grid-area: m;
+	}
+
+	.date-item {
+		grid-area: dt;
+	}
+
+	.info-item {
+		grid-area: i;
+        overflow: hidden;  /* NEW */
+        min-width: 0;      /* NEW; needed for Firefox */
+	}
+
+	.details-item {
+		grid-area: d;
+	}
+
+	.item {
+		grid-column: 1fr;
+	}
+    
+    /* @media not all and (min-width: 1254px) {
+        .responsive-accepted-hidden {
+            display: none !important;
+        }
+    } */
+
+</style>
