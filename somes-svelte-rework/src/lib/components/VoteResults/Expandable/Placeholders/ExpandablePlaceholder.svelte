6373c17d7baff57b1f796eceb7c30c03510e9f8c
elftausend <76885970+elftausend@users.noreply.github.com>
2024-08-13 04:18:08 +0200

    Add placeholder expandable thing

---
+++ somes-svelte-rework/src/lib/components/VoteResults/Expandable/Placeholders/ExpandablePlaceholder.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	let clazz = '';
+	export { clazz as class };
+</script>
+
+<section class="card w-full animate-pulse {clazz}">
+	<div class="p-7 space-y-4">
+		<div class="placeholder" />
+		<div class="grid grid-cols-3 gap-8">
+			<div class="placeholder" />
+			<div class="placeholder" />
+			<div class="placeholder" />
+		</div>
+		<div class="grid grid-cols-3 gap-8">
+			<div class="placeholder" />
+			<div class="placeholder" />
+			<div class="placeholder" />
+		</div>
+	</div>
+</section>
