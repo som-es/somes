eb500c487eadfb18869f337bbb13f1fcdd27a127
elftausend <76885970+elftausend@users.noreply.github.com>
2024-08-17 03:36:09 +0200

    Fix same color for emphasis list

--- somes-svelte-rework/src/lib/components/VoteResults/Emphasis/Emphasis.svelte
+++ somes-svelte-rework/src/lib/components/VoteResults/Emphasis/Emphasis.svelte
@@ -6,10 +6,10 @@
 	{#if emphasis.length > 0}
 		<div class="emphasis-item rounded-xl bg-primary-300 dark:bg-primary-500 px-10 pt-3 pb-3">
 			<h1 class="font-bold text-2xl">Schwerpunkte</h1>
-			<ul class="mt-1 list fill-primary-400">
+			<ul class="mt-1 list fill-primary-400 ">
 				{#each emphasis as emph}
 					<li>
-						<span class="badge bg-primary-500"></span>
+						<span class="badge bg-primary-500 dark:bg-primary-300"></span>
 						<span>{emph}</span>
 					</li>
 				{/each}
