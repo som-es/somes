b8a5b841b3754b30bd1b26aba077a3dfbaec6870
elftausend <76885970+elftausend@users.noreply.github.com>
2024-09-16 23:43:19 +0200

    Update build

--- somes-api/build/_app/immutable/chunks/goto.BHfVe3d4.js
+++
@@ -1,1 +0,0 @@
-import{g as t,b as a}from"./entry.BBoCpMop.js";import{h as r}from"./VoteParliament.C9RIBEHY.js";function g(o,s=!1){r.set(s),t(`${a}${o}`)}export{g};
---
+++ somes-api/build/_app/immutable/entry/start.CKnuu6Lc.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Dgq83e_z.js";export{t as start};
