64b2c7dd2d256db10f79eec4293c745a874362cd
elftausend <76885970+elftausend@users.noreply.github.com>
2024-11-21 18:23:38 +0100

    Add qa

---
+++ somes-api/src/routes/delegates/qa.rs
@@ -0,0 +1,14 @@
+use somes_common_lib::DelegateQA;
+use sqlx::{query_as, PgPool};
+
+pub async fn extract_delegate_qa(
+    delegate_id: i32,
+    pg: &PgPool,
+) -> sqlx::Result<Vec<DelegateQA>> {
+    query_as!(DelegateQA, "select 
+        question, answer 
+        from introduction_transcriptions 
+        inner join transcriptionsqa 
+        on introduction_transcriptions.id = transcriptionsqa.transcription_id where delegate_id = $1", 
+    delegate_id).fetch_all(pg).await
+}
