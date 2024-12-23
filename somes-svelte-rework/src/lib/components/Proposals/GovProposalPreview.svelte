adb7dafb95264f9736b241ad777e8d1b22ae60af
elftausend <76885970+elftausend@users.noreply.github.com>
2024-12-23 17:34:28 +0100

    Add gov proposal stuff

---
+++ somes-svelte-rework/src/lib/components/Proposals/GovProposalPreview.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { GovProposal } from '$lib/types';
+	import GovProposalExpandableBar from './Expandable/GovProposalExpandableBar.svelte';
+
+	export let govProposals: GovProposal[];
+
+	$: previewGovProposals = govProposals.slice(0, 2);
+</script>
+
+<div class="flex flex-wrap justify-between items-center">
+	<h1 class="font-bold text-2xl">Ministerialentwürfe</h1>
+	<button class="btn btn-lg variant-filled" on:click={() => ''}>Alle anzeigen</button>
+</div>
+{#each previewGovProposals as govProposal}
+	<GovProposalExpandableBar {govProposal} />
+{/each}
