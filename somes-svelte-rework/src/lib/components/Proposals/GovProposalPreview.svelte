d26d66a2abeeeffce84db5d109ba9e88b9b36660
elftausend <76885970+elftausend@users.noreply.github.com>
2024-12-30 14:46:42 +0100

    Add fitting filters

--- somes-svelte-rework/src/lib/components/Proposals/GovProposalPreview.svelte
+++ somes-svelte-rework/src/lib/components/Proposals/GovProposalPreview.svelte
@@ -1,15 +1,24 @@
 <script lang="ts">
 	import type { GovProposal } from '$lib/types';
+	import { getModalStore, type ModalSettings } from '@skeletonlabs/skeleton';
 	import GovProposalExpandableBar from './Expandable/GovProposalExpandableBar.svelte';
 
 	export let govProposals: GovProposal[];
+
+	$: allProposals = {
+		type: 'component',
+		component: 'allGovProposals',
+		meta: { govProposals: govProposals }
+	} as ModalSettings;
+
+	const modalStore = getModalStore();
 
 	$: previewGovProposals = govProposals.slice(0, 2);
 </script>
 
 <div class="flex flex-wrap justify-between items-center">
 	<h1 class="font-bold text-2xl">Ministerialentwürfe</h1>
-	<button class="btn btn-lg variant-filled" on:click={() => ''}>Alle anzeigen</button>
+	<button class="btn btn-lg variant-filled" on:click={() => modalStore.trigger(allProposals)}>Alle anzeigen</button>
 </div>
 {#each previewGovProposals as govProposal}
 	<GovProposalExpandableBar {govProposal} />
