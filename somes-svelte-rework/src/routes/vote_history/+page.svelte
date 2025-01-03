526c3c70f5d384955fee75606ef322de6db901f9
elftausend <76885970+elftausend@users.noreply.github.com>
2025-01-03 20:46:53 +0100

    Add Container to vote history

--- somes-svelte-rework/src/routes/vote_history/+page.svelte
+++ somes-svelte-rework/src/routes/vote_history/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { cachedDelegates, filteredDelegates } from '$lib/caching/delegates';
+	import Container from '$lib/components/Layout/Container.svelte';
 	import PaginationVoteResults from '$lib/components/VoteResults/Expandable/PaginationVoteResults.svelte';
 	import type { Delegate, VoteResult } from '$lib/types';
 	import { onMount } from 'svelte';
@@ -10,10 +11,12 @@
 	});
 </script>
 
-<div class="mx-auto px-5">
+<!-- <div class="mx-auto px-5"> -->
+<Container>
 	<h1 class="text-2xl sm:text-4xl font-bold">Vergangene Abstimmungsergebnisse</h1>
 
 	{#if dels}
 		<PaginationVoteResults {dels} />
 	{/if}
-</div>
+</Container>
+<!-- </div> -->
