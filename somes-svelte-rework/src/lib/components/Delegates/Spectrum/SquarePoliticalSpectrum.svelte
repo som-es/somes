a11a725b565d71f5797e5dc364420cbc6b3ecb0c
elftausend <76885970+elftausend@users.noreply.github.com>
2025-01-26 22:20:45 +0100

    update info

--- somes-svelte-rework/src/lib/components/Delegates/Spectrum/SquarePoliticalSpectrum.svelte
+++ somes-svelte-rework/src/lib/components/Delegates/Spectrum/SquarePoliticalSpectrum.svelte
@@ -37,6 +37,6 @@
 <div class="!z-50 card p-4 w-72 shadow-xl" data-popup="politicalPositionHint">
 	<div class="z-50 font-bold text-xl">
         Die Einordnung der politschen Position ist eine grobe Schätzung und muss nicht der Realität entsprechen. 
-        Verwendet wurden AI generierte Antworten zu Fragen, die unter 'Details' abrufbar sind.
+        Verwendet wurden AI generierte Antworten zu Fragen, die nicht immer vollständig korrekt sind und unter 'Details' abrufbar sind.
 	</div>
 </div>
