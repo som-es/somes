2201741cb5b998ef74443c767cfcb44ec6577c5d
elftausend <76885970+elftausend@users.noreply.github.com>
2025-02-06 15:41:03 +0100

    Update websockets perf test!

--- somes-utils/websocket-perf-test.py
+++ somes-utils/websocket-perf-test.py
@@ -4,13 +4,13 @@ import random
 import requests
 
 WS_URL = "ws://somes.at:3000/quiz_room"  
-NUM_CONNECTIONS = 800
+NUM_CONNECTIONS = 1200
 
 async def connect_and_send(index):
-    await asyncio.sleep((random.random() + 0.02) * 50) 
+    await asyncio.sleep((random.random() + 0.02) * 20) 
     print(f"wake up {index}")
-    x = requests.get("http://somes.at:3000/delegates")
-    x.json()
+    # x = requests.get("http://somes.at:3000/delegates")
+    # x.json()
     print(f"JSON ready: {index}")
     async with websockets.connect(WS_URL, open_timeout=60) as websocket:
         await websocket.send("b")
@@ -19,6 +19,16 @@ async def connect_and_send(index):
 
         # await websocket.send("n")
         # response = await websocket.recv()
+
+
+
+        await asyncio.sleep(37)  
+        for _ in range(10000):
+            await asyncio.sleep((random.random() + 0.02) * 10) 
+            num = random.randrange(1, 5)
+            await websocket.send(f"a{num}")
+
+            await asyncio.sleep(10)  
         # print(f"Connection {index}: {response}")
         await asyncio.sleep(60)  
 
