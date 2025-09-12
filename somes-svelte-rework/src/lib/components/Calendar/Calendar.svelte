d97930b13324edac535643b0b576197a287abb1b
elftausend <76885970+elftausend@users.noreply.github.com>
2025-09-12 15:10:15 +0200

    Add PlenarCalendar

---
+++ somes-svelte-rework/src/lib/components/Calendar/Calendar.svelte
@@ -0,0 +1,141 @@
+<script lang="ts">
+	import type { Day } from './types';
+
+	export let headers: string[] = []; 
+	export let days: Day[] = [];
+    export let title: string;
+    export let month: number;
+    export let year: number;
+
+	let days2D: Day[][] = [];
+
+	$: {
+		days2D = [];
+		let acc: Day[] = [];
+		days.forEach((day, i) => {
+			if (i > 0 && i % 5 === 0) {
+				days2D.push([...acc]);
+				acc = [];
+			}
+			acc.push(day);
+		});
+		if (acc.length > 0) {
+			for (let i = 0; i < 4 - acc.length; i++) {
+				days2D.push();
+			}
+			days2D.push(acc);
+		}
+	}
+	function next() {
+		month++;
+		if (month == 12) {
+			year++;
+			month=0;
+		}
+	}
+	function prev() {
+		if (month==0) {
+			month=11;
+			year--;
+		} else {
+			month--;
+		}
+    }
+</script>
+
+<div class="calendar max-w-[50rem] bg-primary-100 dark:bg-primary-600">
+	<div class="flex flex-col">
+        <div class="flex items-center justify-around">
+            <button class="px-5" on:click={() => year--}>{"<<"} </button>
+            <button class="px-5" on:click={prev}>{"<"}</button>
+            <span class="text-3xl text-pretty font-bold text-center">
+                {title}
+            </span>
+            <button class="px-5" on:click={next}>{">"}</button>
+            <button class="px-5" on:click={() => year++}>{">>"}</button>
+        </div>
+        
+		<div class="flex flex-row">
+			{#each headers as header}
+				<span class="day-name text-secondary-500">
+					{header}
+				</span>
+			{/each}
+		</div>
+
+		{#each days2D as week}
+			<div class="flex flex-row">
+				{#each Array(5) as _, i}
+					{#if week[i]}
+						{#if week[i].enabled}
+							<div class="flex-1 flex items-center">
+                                {#if week[i].item !== null}
+								    <span class="{week[i].item.classes}">{week[i].item.title}</span>
+                                {/if}
+								<div class="day">{week[i].name}</div>
+							</div>
+						{:else}
+							<div class="day day-disabled">{week[i].name}</div>
+						{/if}
+					{:else}
+						<div class="flex-1">
+							<div class="day empty-day h-full"></div>
+						</div>
+					{/if}
+				{/each}
+			</div>
+		{/each}
+	</div>
+</div>
+
+<style>
+	.calendar {
+		overflow: auto;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.flex-row {
+		display: flex;
+		flex-direction: row;
+	}
+
+	.day {
+		border-bottom: 1px solid rgba(166, 168, 179, 0.12);
+		border-right: 1px solid rgba(166, 168, 179, 0.12);
+		text-align: right;
+		padding: 14px 20px;
+		font-size: 14px;
+		color: #98a0a6;
+		flex: 1; /* evenly distribute 5 days per row */
+		box-sizing: border-box;
+	}
+
+	.day-name {
+		font-size: 12px;
+		text-transform: uppercase;
+		text-align: center;
+		border-bottom: 1px solid rgba(166, 168, 179, 0.12);
+		line-height: 50px;
+		font-weight: 500;
+		flex: 1; /* evenly distribute headers */
+	}
+
+	.day-disabled {
+		color: rgba(152, 160, 166, 0.5);
+		background-color: #fff;
+		cursor: not-allowed;
+	}
+
+	.task {
+		border-left-width: 3px;
+		padding: 8px 12px;
+		margin: 10px;
+		border-left-style: solid;
+		font-size: 14px;
+		position: relative;
+		align-self: center;
+		z-index: 2;
+		border-radius: 15px;
+	}
+</style>
---
+++ somes-svelte-rework/src/lib/components/Calendar/PlenarCalendar.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import Calendar from "./Calendar.svelte";
+	import type { Day } from "./types";
+
+	const date = new Date();
+	let year = date.getFullYear();
+	let month = date.getMonth();
+
+	var daysInThisMonth = new Date(year, month+1, 0).getDate();
+
+    const months = ["Jänner", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
+
+	let days: Day[] = []
+
+	const addDays = () => {
+        days = []
+        for (let i = 0; i < daysInThisMonth; i++) {
+            let testDate = new Date(year,month,i+1);
+            console.log(testDate);
+            console.log(testDate.getDay());
+            if (testDate.getDay() > 0 && testDate.getDay() < 6) {
+                days.push({name: (i +1).toString(), enabled: true, date: testDate, item: null})
+            }
+        }
+
+        days[17].item = { title: "Plenarsitzung", classes: "badge text-xs bg-tertiary-500"}
+        days[18].item = { title: "Plenarsitzung", classes: "badge text-xs bg-tertiary-500"}
+    }
+
+    $: month, year,addDays();
+
+</script>
+
+{#if days}
+    <Calendar 
+        bind:month 
+        bind:year 
+        title={`${months[month % 12]} ${year}`} {days} 
+        headers={["Mo", "Di", "Mi", "Do", "Fr"]} 
+    />
+{/if}
