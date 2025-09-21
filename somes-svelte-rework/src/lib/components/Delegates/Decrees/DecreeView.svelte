bfb2c9e7e6d3dae873d5b74096a520a231b45d8a
elftausend <76885970+elftausend@users.noreply.github.com>
2025-09-21 20:58:01 +0200

    Add external link to decree ris

--- somes-svelte-rework/src/lib/components/Delegates/Decrees/DecreeView.svelte
+++ somes-svelte-rework/src/lib/components/Delegates/Decrees/DecreeView.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import Documents from "$lib/components/Documents/Documents.svelte";
-import Emphasis from "$lib/components/VoteResults/Emphasis/Emphasis.svelte";
+    import Emphasis from "$lib/components/VoteResults/Emphasis/Emphasis.svelte";
 	import DecreeInfoTiles from "$lib/components/VoteResults/InfoTiles/DecreeInfoTiles.svelte";
 	import { dashDateToDotDate } from "$lib/date";
-import type { Delegate } from "$lib/types";
+    import type { Delegate } from "$lib/types";
 	import DelegateCard from "../DelegateCard.svelte";
 	import type { Decree } from "./types";
+    import externalLink from "$lib/assets/misc_icons/external-link.svg?raw";
 
     export let decree: Decree; 
     export let delegate: Delegate | null; 
@@ -21,13 +22,18 @@ import type { Delegate } from "$lib/types";
 <div class="entry bg-primary-200 dark:bg-primary-400 mt-3 flex max-lg:flex-wrap gap-3">
     <div class="flex flex-col gap-3 w-full">
         <div class="rounded-xl bg-primary-300 dark:bg-primary-500 px-3 py-3">
-            <div class="flex justify-between items-center">
+            <div class="flex justify-between ">
                 <div>
 					<h1 class="font-bold text-xl md:text-3xl">
                         Verordnung
                     </h1>
                     <span class="md:text-xl">{decree.short_title}</span>
-                </div>
+                </div> 
+                {#if decree.document_url}
+                    <a class="w-10" href={decree.document_url} target="_blank">
+                        {@html externalLink}
+                    </a>
+                {/if}
             </div>
         </div>
         {#if decree.emphasis}
