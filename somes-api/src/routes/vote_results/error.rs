0237115d075805f66044b87e918efeadf4c3c6e2
elftausend <76885970+elftausend@users.noreply.github.com>
2025-12-06 14:45:51 +0100

    Move vote results

---
+++ somes-api/src/routes/vote_results/error.rs
@@ -0,0 +1,49 @@
+use axum::{response::IntoResponse, Json};
+use reqwest::StatusCode;
+use serde_json::json;
+use utoipa::ToSchema;
+
+use crate::GenericErrorResponse;
+
+#[derive(Debug)]
+pub enum LegisInitErrorResponse {
+    LegisInit,
+    LatestLegisInit,
+    LatestVoteResults,
+    VoteResultById,
+    InvalidPage,
+    GenericErrorResponse(GenericErrorResponse),
+}
+
+impl IntoResponse for LegisInitErrorResponse {
+    fn into_response(self) -> axum::response::Response {
+        let (status_code, err_msg) = match self {
+            LegisInitErrorResponse::LegisInit => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "could not return legislative initiatives",
+            ),
+            LegisInitErrorResponse::LatestLegisInit => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "could not return latest legislative initiatives",
+            ),
+            LegisInitErrorResponse::LatestVoteResults => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "could not return latest vote results",
+            ),
+            LegisInitErrorResponse::InvalidPage => (StatusCode::BAD_REQUEST, "invalid page"),
+            LegisInitErrorResponse::VoteResultById => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "could not return legislative init by id",
+            ),
+            LegisInitErrorResponse::GenericErrorResponse(generic_error_response) => {
+                return generic_error_response.into_response()
+            }
+        };
+
+        let body = Json(json!({
+            "error": err_msg,
+        }));
+
+        (status_code, body).into_response()
+    }
+}
