d80ef70ac69afbce10b63a4cd0066180211f6da0
elftausend <76885970+elftausend@users.noreply.github.com>
2025-12-27 15:45:44 +0100

    Add InfoBadgesCustom.svelte

---
+++ somes-svelte-rework/src/lib/components/VoteResults/InfoTiles/InfoBadgesCustom.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { dashDateToDotDate } from "$lib/date";
+    export let texts: (string | null)[];
+</script>
+<div class="flex max-sm:flex-wrap gap-1 mt-1">
+    {#each texts as text}
+        {#if text}
+            <span class="badge bg-tertiary-400 text-black">{text}</span>
+        {/if}
+    {/each}
+</div>
