b88700111862ced8a6dad714053719f5c4c7ab65
elftausend <76885970+elftausend@users.noreply.github.com>
2026-01-04 20:23:32 +0100

    Add tailwind css vite plugin

--- somes-svelte-rework/vite.config.ts
+++ somes-svelte-rework/vite.config.ts
@@ -1,10 +1,14 @@
+import tailwindcss from '@tailwindcss/vite'
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 // the host: '0.0.0.0' setting, exposes the app to all devices on the net 
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		tailwindcss(),
+		sveltekit()
+	],
 	server: {
 		host: '0.0.0.0'
 	}
