9f526cfbf21e970ac24d8ae200545dafbdd7ba1b
Fabi <fabian.cemerka@gmx.at>
2026-03-04 23:22:11 +0100

    fixed open drawer after oauth login

--- somes-frontend/src/routes/user/+page.svelte
+++ somes-frontend/src/routes/user/+page.svelte
@@ -3,12 +3,13 @@
 	import { page } from '$app/state';
 	import { goto } from '$app/navigation';
 	import { resolve } from '$app/paths';
-	import { jwtStore } from '$lib/caching/stores/stores.svelte';
+	import { jwtStore, loginDrawerOpenStore } from '$lib/caching/stores/stores.svelte';
 
 	onMount(() => {
 		const token = page.url.searchParams.get('token');
 		if (token) {
 			jwtStore.value = token;
+			loginDrawerOpenStore.value = false;
 			goto(resolve('/user'), { replaceState: true });
 		}
 	});
