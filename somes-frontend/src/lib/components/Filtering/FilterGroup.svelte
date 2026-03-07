e318d34070912476e2788814d2a460081bf22bc1
elftausend <76885970+elftausend@users.noreply.github.com>
2026-03-07 15:49:20 +0000

    Fix some dark mode

--- somes-frontend/src/lib/components/Filtering/FilterGroup.svelte
+++ somes-frontend/src/lib/components/Filtering/FilterGroup.svelte
@@ -11,12 +11,11 @@
 
 {#if !group.hidden}
     <div class="mt-4 first:mt-0">
-        <span class="text-base font-semibold text-gray-800">{group.title}</span>
-        <div class="flex w-fit gap-1 rounded-lg border border-primary-300 text-sm">
+        <span class="text-base font-semibold text-gray-800 dark:text-gray-50">{group.title}</span>
+        <div class="flex w-fit gap-1 rounded-lg border border-primary-300 dark:border-primary-400 text-sm">
             {#each group.options as option}
                 <button
-                    class="cursor-pointer rounded-lg px-2 py-1 text-xs md:text-sm"
-                    class:bg-primary-300={group.activeValue === option.value}
+                    class="cursor-pointer rounded-lg px-2 py-1 text-xs md:text-sm {group.activeValue === option.value ? "bg-primary-300 dark:bg-primary-400" : ""}"
                     onclick={() => {
                         group.activeValue = option.value;
                     }}
