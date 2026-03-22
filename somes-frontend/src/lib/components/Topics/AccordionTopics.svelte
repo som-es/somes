f2be4fe98f9172005a5725c2221e6c6d21894739
elftausend <76885970+elftausend@users.noreply.github.com>
2026-03-22 20:02:35 +0100

    Add down arrow to topic accordion

--- somes-frontend/src/lib/components/Topics/AccordionTopics.svelte
+++ somes-frontend/src/lib/components/Topics/AccordionTopics.svelte
@@ -5,6 +5,7 @@
 	import DisplayTopic from './Topic.svelte';
 	import { addUserTopic, removeUserTopic } from '$lib/api/authed';
 	import { cachedUserTopics } from '$lib/caching/user_topics_cache.svelte';
+	import arrowDown from '$lib/assets/misc_icons/down-arrow.svg?raw';
 
 	interface Props {
 		parentTopics: string[];
@@ -33,20 +34,23 @@
 
 <Accordion.Root type="multiple">
 	<Accordion.Item value={parentTopics.join('-')}>
-		<Accordion.Trigger class="flex w-full items-center gap-1 rounded-lg px-2 py-2 text-left hover:bg-primary-200 dark:hover:bg-primary-600">
-			{#each parentTopics as parentTopic}
+		<Accordion.Trigger
+			class="flex w-full flex-wrap items-center justify-between gap-1 rounded-lg px-2 py-2 text-left hover:bg-primary-200 dark:hover:bg-primary-600 [&[data-state=open]>span>svg]:rotate-180"
+		>
+			{#each parentTopics as parentTopic (parentTopic)}
 				<span class="rounded-md bg-primary-400 px-2 py-0.5 text-sm font-bold dark:bg-primary-700">
 					{parentTopic}
 				</span>
 			{/each}
+			<span class="w-5">{@html arrowDown}</span>
 		</Accordion.Trigger>
 
 		<Accordion.Content>
 			<div class="flex flex-wrap gap-2 px-2 py-2">
-				{#each subTopics as topic}
+				{#each subTopics as topic (topic.id)}
 					{#if selectedTopics.has(topic.id)}
 						<ClickableSpan action={() => removeFromSelection(topic)}>
-							<DisplayTopic class={'!bg-secondary-400'}>
+							<DisplayTopic class="bg-secondary-400!">
 								{topic.topic}
 							</DisplayTopic>
 						</ClickableSpan>
