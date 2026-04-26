eb10f47e025954f65a31f6b3e1cef2d9806bfba5
elftausend <76885970+elftausend@users.noreply.github.com>
2026-04-26 17:03:09 +0200

    Add NewBadge

---
+++ somes-frontend/src/lib/components/UI/NewBadge.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { isEntryNew } from '$lib/date';
+
+	interface Props {
+		date: string;
+	}
+	const { date }: Props = $props();
+</script>
+
+{#if isEntryNew(new Date(date))}
+	<span class="badge bg-secondary-200 text-black">Neu</span>
+{/if}
