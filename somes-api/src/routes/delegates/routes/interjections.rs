e7f14655be36fa746ac2dcfdf4fe2533e3633782
elftausend <76885970+elftausend@users.noreply.github.com>
2026-05-01 14:24:01 +0200

    Add interjections per page routes

--- somes-api/src/routes/delegates/routes/interjections.rs
+++ somes-api/src/routes/delegates/routes/interjections.rs
@@ -1,1 +1,44 @@
+use axum::{extract::Query, Json};
+use somes_common_lib::DelegateByIdAndPage;
 
+use crate::{
+    routes::{
+        extract_interjections_made_by_delegate, extract_interjections_received_by_delegate,
+        DelegateError, InterjectionsWithMaxPage,
+    },
+    PgPoolConnection,
+};
+
+pub async fn interjections_made_by_delegate_per_page_route(
+    PgPoolConnection(pg): PgPoolConnection,
+    Query(delegate_by_id_and_page): Query<DelegateByIdAndPage>,
+) -> Result<Json<InterjectionsWithMaxPage>, DelegateError> {
+    let DelegateByIdAndPage { delegate_id, page } = delegate_by_id_and_page;
+
+    let page_elements = std::env::var("INTERJECTIONS_PER_PAGE")
+        .unwrap_or_else(|_| "20".to_string())
+        .parse()
+        .unwrap_or(20);
+    Ok(
+        extract_interjections_made_by_delegate(delegate_id, page, page_elements, &pg)
+            .await
+            .map(Json)?,
+    )
+}
+
+pub async fn interjections_received_by_delegate_per_page_route(
+    PgPoolConnection(pg): PgPoolConnection,
+    Query(delegate_by_id_and_page): Query<DelegateByIdAndPage>,
+) -> Result<Json<InterjectionsWithMaxPage>, DelegateError> {
+    let DelegateByIdAndPage { delegate_id, page } = delegate_by_id_and_page;
+
+    let page_elements = std::env::var("INTERJECTIONS_PER_PAGE")
+        .unwrap_or_else(|_| "20".to_string())
+        .parse()
+        .unwrap_or(20);
+    Ok(
+        extract_interjections_received_by_delegate(delegate_id, page, page_elements, &pg)
+            .await
+            .map(Json)?,
+    )
+}
