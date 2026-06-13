52f9e2495318eb551f05c8f8b5a0e6e3f78d8a7a
elftausend <76885970+elftausend@users.noreply.github.com>
2026-06-13 23:14:32 +0200

    Add basic pqa extract fns

--- somes-api/src/routes/delegates/parliamentary_qa.rs
+++ somes-api/src/routes/delegates/parliamentary_qa.rs
@@ -1,5 +1,36 @@
 use sqlx::PgPool;
 
-pub fn extract_parliamentary_questions(pg: &PgPool, delegate_id: i32) {}
+use combx::models::*;
 
-pub fn extract_parliamentary_answers(pg: &PgPool, delegate_id: i32) {}
+pub async fn extract_parliamentary_questions(
+    pg: &PgPool,
+    delegate_id: i32,
+) -> sqlx::Result<Vec<ParliamentQuestionResponse>> {
+    sqlx::query_as!(
+        ParliamentQuestionResponse,
+        r#"
+        SELECT *
+        FROM pqa_composite_questions
+        WHERE $1 = ANY((("question: ParliamentQuestion").data).issuer_ids)
+        "#,
+        delegate_id as i64
+    )
+    .fetch_all(pg)
+    .await
+}
+pub async fn extract_parliamentary_answers(
+    pg: &PgPool,
+    delegate_id: i32,
+) -> sqlx::Result<Vec<ParliamentQuestionResponse>> {
+    sqlx::query_as!(
+        ParliamentQuestionResponse,
+        r#"
+        SELECT *
+        FROM pqa_composite_questions
+        WHERE $1 = ANY((("question: ParliamentQuestion").data).receiver_ids)
+        "#,
+        delegate_id as i64
+    )
+    .fetch_all(pg)
+    .await
+}
